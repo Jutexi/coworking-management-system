ff69b58c2a3d781b96565533d9aa75cf5665c009
yan <tank22899@gmail.com>
2025-09-16 05:52:33 +0300

    sonar. back ещ

--- src/main/java/com/app/coworking/aspect/LoggingAspect.java
+++ src/main/java/com/app/coworking/aspect/LoggingAspect.java
@@ -23,8 +23,10 @@ public class LoggingAspect {
         String className = joinPoint.getTarget().getClass().getSimpleName();
         Object[] args = joinPoint.getArgs();
 
-        logger.info("Controller method {}.{}() called with args: {}",
-                className, methodName, Arrays.toString(args));
+        if (logger.isInfoEnabled()) {
+            logger.info("Controller method {}.{}() called with args: {}",
+                    className, methodName, Arrays.toString(args));
+        }
 
         try {
             Object result = joinPoint.proceed();
@@ -32,7 +34,6 @@ public class LoggingAspect {
                     className, methodName);
             return result;
         } catch (Exception e) {
-            logger.error("Error in {}.{}(): {}", className, methodName, e.getMessage());
             throw e;
         }
     }
