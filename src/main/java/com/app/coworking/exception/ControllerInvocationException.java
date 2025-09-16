5fab3182f2ce632d6061037afa9c36d402fb45ee
yan <tank22899@gmail.com>
2025-09-16 04:39:01 +0300

    sonar.8

--- src/main/java/com/app/coworking/exception/ControllerInvocationException.java
+++
@@ -1,9 +0,0 @@
-package com.app.coworking.exception;
-
-public class ControllerInvocationException extends RuntimeException {
-
-    public ControllerInvocationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
